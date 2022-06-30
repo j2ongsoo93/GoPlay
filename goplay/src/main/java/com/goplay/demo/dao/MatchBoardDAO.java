048e2b83f6c2e9de4e2187de9c7aebca0206e3c4
j2ongsoo93 <j2ongsoo93@gmail.com>
2022-06-30 15:09:27 +0900

    [박정수] 추가: Querydsl dependency

--- goplay/src/main/java/com/goplay/demo/dao/MatchBoardDAO.java
+++ goplay/src/main/java/com/goplay/demo/dao/MatchBoardDAO.java
@@ -1,17 +1,32 @@
 package com.goplay.demo.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.goplay.demo.vo.MatchBoard;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
-public interface MatchBoardDAO extends JpaRepository<MatchBoard, Integer> {
-	public List<MatchBoard> findAllByOrderByMbDate();
+public interface MatchBoardDAO extends JpaRepository<MatchBoard, Integer>{
+	List<MatchBoard> findAllByOrderByMbDate();
 	
-	public List<MatchBoard> findByMbStat(@Param("mb_stat") String mb_stat);
+	List<MatchBoard> findByMbStat(@Param("mb_stat") String mb_stat);
 	
-	public List<MatchBoard> findByMbStatAndMbType(@Param("mb_stat") String mb_stat, @Param("mb_type") String mb_type);
+	List<MatchBoard> findByMbStatAndMbType(@Param("mb_stat") String mb_stat, @Param("mb_type") String mb_type);
+
+	List<MatchBoard> findByMbDateAndMbType(@Nullable Date mbDate, String mbType);
 	
+	//검색 메서드
+	List<MatchBoard> findByMbDateAndMbTypeAndMbLoc1AndMbLoc2AndMbStatOrderByMbDateAsc(@Nullable Date mbDate, @Nullable String mbType, @Nullable String mbLoc1, @Nullable String mbLoc2, @Nullable String mbStat);
+
+
+
+
+
+
+
 }
--- goplay/src/main/java/com/goplay/demo/service/MatchBoardService.java
+++ goplay/src/main/java/com/goplay/demo/service/MatchBoardService.java
@@ -1,8 +1,10 @@
 package com.goplay.demo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import com.goplay.demo.dao.MatchBoardDAO;
@@ -15,7 +17,7 @@ public class MatchBoardService {
 	@Autowired
 	private MatchBoardDAO dao;
 	
-	public List<MatchBoard> listMatchboard(){
+	public List<MatchBoard> listMatchBoard(){
 		return dao.findAllByOrderByMbDate();
 	}
 	
@@ -30,5 +32,10 @@ public class MatchBoardService {
 	//매치등록
 	public void saveBoard(MatchBoard mb) {
 		dao.save(mb);
+	}
+
+	//매치검색
+	public List<MatchBoard> findMatchBoard(@Nullable Date mbDate, @Nullable String mbType, @Nullable String mbLoc1, @Nullable String mbLoc2, @Nullable String mbStat){
+		return dao.findByMbDateAndMbTypeAndMbLoc1AndMbLoc2AndMbStatOrderByMbDateAsc(mbDate, mbType, mbLoc1, mbLoc2, mbStat);
 	}
 }
