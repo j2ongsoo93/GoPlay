a69cb20ee579dd884ed51b7e2a40bd805805e7ba
kikk789 <kakk789@naver.com>
2022-07-11 11:57:03 +0900

    [이창희] 이창희 파일 복구 완료

--- goplay/src/main/java/com/goplay/demo/GoplayApplication.java
+++ goplay/src/main/java/com/goplay/demo/GoplayApplication.java
@@ -1,6 +1,5 @@
 package com.goplay.demo;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
